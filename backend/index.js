0c39aeb113eb3673b5737b636e16eb1f452991c8
gethushan <gethushan@gmail.com>
2024-03-16 18:02:28 +0530

    changing index js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const port = 4000
 const mongoose = require('mongoose')
 const userRouter = require("./userRouter")
 const cors = require('cors')
@@ -9,16 +8,18 @@ const morgan = require('morgan')
 require('dotenv').config()
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(cors())
-
+app.use(cors(
+  {
+      origin: 'https://login-project-flame.vercel.app',
+      methods: ["GET,POST"],
+      credentials: true
+}))
 
 mongoose.set('strictQuery',true);
 mongoose.connect(process.env.DATABASE_CONNECTION_VARIABLE)
 app.use('/j',userRouter)
 
-
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(4000, () => {
+    console.log(`Example app listening on port 4000`)
   })
 
